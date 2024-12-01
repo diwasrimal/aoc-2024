194f3e7bc37112d733b7b25b9b8805753c28986c
Diwas Rimal <rimaldiwas10@gmail.com>
2024-12-01 13:04:24 +0545

    part 1 done

---
+++ 01/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+var list1, list2 []int
+var list2freq = make(map[int]int)
+
+func main() {
+	part := flag.Int("part", 0, "part of the problem (1 or 2)")
+	input := flag.String("input", "", "input file")
+	flag.Parse()
+
+	if (*part != 1 && *part != 2) || *input == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	file, err := os.Open(*input)
+	if err != nil {
+		fmt.Printf("Error opening %s: %s\n", *input, err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+	for sc.Scan() {
+		line := sc.Text()
+		first, second, _ := strings.Cut(line, "   ")
+		val1, err := strconv.Atoi(first)
+		if err != nil {
+			fmt.Println("strconv.Atoi() errord")
+			os.Exit(1)
+		}
+		val2, err := strconv.Atoi(second)
+		if err != nil {
+			fmt.Println("strconv.Atoi() errord")
+			os.Exit(1)
+		}
+		list1 = append(list1, val1)
+		list2 = append(list2, val2)
+		list2freq[val2]++
+	}
+
+	if *part == 1 {
+		part1()
+	} else {
+		part2()
+	}
+}
+
+func part1() {
+	slices.Sort(list1)
+	slices.Sort(list2)
+
+	sum := 0
+	for i := 0; i < len(list1); i++ {
+		diff := list1[i] - list2[i]
+		if diff < 0 {
+			diff = -diff
+		}
+		sum += diff
+	}
+
+	fmt.Println("part1", sum)
+}
+
+func part2() {
+	sum := 0
+	for _, val := range list1 {
+		sum += val * list2freq[val]
+	}
+	fmt.Println("part2", sum)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/diwasrimal/aoc-2024-go
+
+go 1.23.3
